f26d98c23d0a02af49547d3a73837f3dd4fcea3d
Cory Bennett <cbennett@netflix.com>
2017-08-29 13:48:13 -0700

    first commit

---
+++ alias.go
@@ -0,0 +1,105 @@
+package kingpeon
+
+import (
+	"fmt"
+	"strings"
+)
+
+type AliasOpt struct {
+	Name     string         `yaml:"name,omitempty" json:"name,omitempty"`
+	Type     AliasValueType `yaml:"type,omitempty" json:"type,omitempty"`
+	Help     string         `yaml:"help,omitempty" json:"help,omitempty"`
+	Short    string         `yaml:"short,omitempty" json:"short,omitempty"`
+	Required bool           `yaml:"required,omitempty" json:"required,omitempty"`
+	Default  interface{}    `yaml:"default,omitempty" json:"default,omitempty"`
+	Hidden   bool           `yaml:"hidden,omitempty" json:"hidden,omitempty"`
+	Repeat   bool           `yaml:"repeat,omitempty" json:"repeat,omitempty"`
+}
+
+type AliasArg struct {
+	Name     string         `yaml:"name,omitempty" json:"name,omitempty"`
+	Help     string         `yaml:"help,omitempty" json:"help,omitempty"`
+	Type     AliasValueType `yaml:"type,omitempty" json:"type,omitempty"`
+	Required bool           `yaml:"required,omitempty" json:"required,omitempty"`
+	Default  interface{}    `yaml:"default,omitempty" json:"default,omitempty"`
+	Repeat   bool           `yaml:"repeat,omitempty" json:"repeat,omitempty"`
+}
+
+type Alias struct {
+	Name     string     `yaml:"name,omitempty" json:"name,omitempty"`
+	Options  []AliasOpt `yaml:"options,omitempty" json:"options,omitempty"`
+	Args     []AliasArg `yaml:"args,omitempty" json:"args,omitempty"`
+	Script   string     `yaml:"script,omitempty" json:"script,omitempty"`
+	Help     string     `yaml:"help,omitempty" json:"help,omitempty"`
+	Default  bool       `yaml:"default,omitempty" json:"default,omitempty"`
+	Hidden   bool       `yaml:"hidden,omitempty" json:"hidden,omitempty"`
+	AltNames []string   `yaml:"alt-names,omitempty" json:"alt-names,omitempty"`
+}
+
+type Aliases []Alias
+
+type AliasValueType int
+
+const (
+	DEFAULT AliasValueType = iota
+	BOOL
+	COUNTER
+	FLOAT32
+	FLOAT64
+	INT8
+	INT16
+	INT32
+	INT64
+	INT
+	STRING
+	STRINGMAP
+	UINT8
+	UINT16
+	UINT32
+	UINT64
+	UINT
+)
+
+func (o *AliasValueType) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var optType string
+	if err := unmarshal(&optType); err != nil {
+		return err
+	}
+	switch strings.ToUpper(optType) {
+	case "BOOL":
+		*o = BOOL
+	case "COUNTER":
+		*o = COUNTER
+	case "FLOAT32":
+		*o = FLOAT32
+	case "FLOAT64":
+		*o = FLOAT64
+	case "INT8":
+		*o = INT8
+	case "INT16":
+		*o = INT16
+	case "INT32":
+		*o = INT32
+	case "INT64":
+		*o = INT64
+	case "INT":
+		*o = INT
+	case "STRING":
+		*o = STRING
+	case "STRINGMAP":
+		*o = STRINGMAP
+	case "UINT8":
+		*o = UINT8
+	case "UINT16":
+		*o = UINT16
+	case "UINT32":
+		*o = UINT32
+	case "UINT64":
+		*o = UINT64
+	case "UINT":
+		*o = UINT
+	default:
+		return fmt.Errorf("Unknown option type: %s", optType)
+	}
+	return nil
+}
---
+++ kingpeon.go
@@ -0,0 +1,558 @@
+package kingpeon
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+	"text/template"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+// variable so we can mock for testing
+var syscallExec = syscall.Exec
+
+func runAlias(alias *Alias, t *template.Template) error {
+	buf := bytes.NewBufferString("")
+	t, err := t.Parse(alias.Script)
+	if err != nil {
+		return err
+	}
+	err = t.Execute(buf, nil)
+	if err != nil {
+		return err
+	}
+
+	bin, err := exec.LookPath("sh")
+	if err != nil {
+		return err
+	}
+	cmd := []string{"sh", "-c", buf.String()}
+	return syscallExec(bin, cmd, os.Environ())
+}
+
+// either kingpin.Application or kingpin.CmdClause fit this interface
+type kingpinAppOrCommand interface {
+	Command(string, string) *kingpin.CmdClause
+	GetCommand(string) *kingpin.CmdClause
+}
+
+func lookupCommand(app *kingpin.Application, alias *Alias) *kingpin.CmdClause {
+	commandWords := strings.Fields(alias.Name)
+	var appOrCmd kingpinAppOrCommand = app
+	if len(commandWords) > 1 {
+		for _, name := range commandWords[0 : len(commandWords)-1] {
+			tmp := appOrCmd.GetCommand(name)
+			if tmp == nil {
+				tmp = appOrCmd.Command(name, "")
+			}
+			appOrCmd = tmp
+		}
+	}
+
+	return appOrCmd.Command(commandWords[len(commandWords)-1], alias.Help)
+}
+
+func RegisterAliases(app *kingpin.Application, aliases Aliases, t *template.Template) error {
+
+	args := map[string]interface{}{}
+	opts := map[string]interface{}{}
+
+	t = t.Funcs(map[string]interface{}{
+		"args": func() map[string]interface{} {
+			return args
+		},
+		"options": func() map[string]interface{} {
+			return opts
+		},
+	})
+
+	for _, alias := range aliases {
+		cmd := lookupCommand(app, &alias)
+		for _, alt := range alias.AltNames {
+			cmd = cmd.Alias(alt)
+		}
+
+		if alias.Default {
+			cmd = cmd.Default()
+		}
+
+		if alias.Hidden {
+			cmd = cmd.Hidden()
+		}
+
+		for _, opt := range alias.Options {
+			cmdFlag := cmd.Flag(opt.Name, opt.Help)
+			if opt.Short != "" {
+				cmdFlag.Short(rune(opt.Short[0]))
+			}
+			if opt.Required {
+				cmdFlag.Required()
+			}
+
+			if opt.Default != "" {
+				opts[opt.Name] = opt.Default
+			}
+
+			if opt.Hidden {
+				cmdFlag.Hidden()
+			}
+
+			switch opt.Type {
+			case BOOL:
+				if opt.Repeat {
+					var val []bool
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).BoolListVar(&val)
+				} else {
+					var val bool
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).BoolVar(&val)
+				}
+			case COUNTER:
+				if opt.Repeat {
+					return fmt.Errorf("`type: COUNTER` and `repeat: true` not supported for %s", opt.Name)
+				} else {
+					var val int
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).CounterVar(&val)
+				}
+			case FLOAT32:
+				if opt.Repeat {
+					var val []float32
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Float32ListVar(&val)
+				} else {
+					var val float32
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Float32Var(&val)
+				}
+			case FLOAT64:
+				if opt.Repeat {
+					var val []float64
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Float64ListVar(&val)
+				} else {
+					var val float64
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Float64Var(&val)
+				}
+			case INT8:
+				if opt.Repeat {
+					var val []int8
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Int8ListVar(&val)
+				} else {
+					var val int8
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Int8Var(&val)
+				}
+			case INT16:
+				if opt.Repeat {
+					var val []int16
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Int16ListVar(&val)
+				} else {
+					var val int16
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Int16Var(&val)
+				}
+			case INT32:
+				if opt.Repeat {
+					var val []int32
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Int32ListVar(&val)
+				} else {
+					var val int32
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Int32Var(&val)
+				}
+			case INT64:
+				if opt.Repeat {
+					var val []int64
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Int64ListVar(&val)
+				} else {
+					var val int64
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Int64Var(&val)
+				}
+			case INT:
+				if opt.Repeat {
+					var val []int
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).IntsVar(&val)
+				} else {
+					var val int
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).IntVar(&val)
+				}
+			case DEFAULT, STRING:
+				if opt.Repeat {
+					var val []string
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).StringsVar(&val)
+				} else {
+					var val string
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).StringVar(&val)
+				}
+			case STRINGMAP:
+				if opt.Repeat {
+					return fmt.Errorf("`type: STRINGMAP` and `repeat: true` not supported for %s", opt.Name)
+				} else {
+					val := make(map[string]string)
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).StringMapVar(&val)
+				}
+			case UINT8:
+				if opt.Repeat {
+					var val []uint8
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Uint8ListVar(&val)
+				} else {
+					var val uint8
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Uint8Var(&val)
+				}
+			case UINT16:
+				if opt.Repeat {
+					var val []uint16
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Uint16ListVar(&val)
+				} else {
+					var val uint16
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Uint16Var(&val)
+				}
+			case UINT32:
+				if opt.Repeat {
+					var val []uint32
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Uint32ListVar(&val)
+				} else {
+					var val uint32
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Uint32Var(&val)
+				}
+			case UINT64:
+				if opt.Repeat {
+					var val []uint64
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Uint64ListVar(&val)
+				} else {
+					var val uint64
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).Uint64Var(&val)
+				}
+			case UINT:
+				if opt.Repeat {
+					var val []uint
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).UintsVar(&val)
+				} else {
+					var val uint
+					cmdFlag.PreAction(func(_ *kingpin.ParseContext) error {
+						opts[opt.Name] = val
+						return nil
+					}).UintVar(&val)
+				}
+			}
+		}
+
+		for _, arg := range alias.Args {
+			cmdArg := cmd.Arg(arg.Name, arg.Help)
+			if arg.Required {
+				cmdArg.Required()
+			}
+			if arg.Default != "" {
+				args[arg.Name] = arg.Default
+			}
+
+			switch arg.Type {
+			case BOOL:
+				if arg.Repeat {
+					var val []bool
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).BoolListVar(&val)
+				} else {
+					var val bool
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).BoolVar(&val)
+				}
+			case COUNTER:
+				if arg.Repeat {
+					return fmt.Errorf("`type: COUNTER` and `repeat: true` not supported for %s", arg.Name)
+				} else {
+					var val int
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).CounterVar(&val)
+				}
+			case FLOAT32:
+				if arg.Repeat {
+					var val []float32
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Float32ListVar(&val)
+				} else {
+					var val float32
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Float32Var(&val)
+				}
+			case FLOAT64:
+				if arg.Repeat {
+					var val []float64
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Float64ListVar(&val)
+				} else {
+					var val float64
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Float64Var(&val)
+				}
+			case INT8:
+				if arg.Repeat {
+					var val []int8
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Int8ListVar(&val)
+				} else {
+					var val int8
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Int8Var(&val)
+				}
+			case INT16:
+				if arg.Repeat {
+					var val []int16
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Int16ListVar(&val)
+				} else {
+					var val int16
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Int16Var(&val)
+				}
+			case INT32:
+				if arg.Repeat {
+					var val []int32
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Int32ListVar(&val)
+				} else {
+					var val int32
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Int32Var(&val)
+				}
+			case INT64:
+				if arg.Repeat {
+					var val []int64
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Int64ListVar(&val)
+				} else {
+					var val int64
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Int64Var(&val)
+				}
+			case INT:
+				if arg.Repeat {
+					var val []int
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).IntsVar(&val)
+				} else {
+					var val int
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).IntVar(&val)
+				}
+			case DEFAULT, STRING:
+				if arg.Repeat {
+					var val []string
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).StringsVar(&val)
+				} else {
+					var val string
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).StringVar(&val)
+				}
+			case STRINGMAP:
+				if arg.Repeat {
+					return fmt.Errorf("`type: STRINGMAP` and `repeat: true` not supported for %s", arg.Name)
+				} else {
+					val := make(map[string]string)
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).StringMapVar(&val)
+				}
+			case UINT8:
+				if arg.Repeat {
+					var val []uint8
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Uint8ListVar(&val)
+				} else {
+					var val uint8
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Uint8Var(&val)
+				}
+			case UINT16:
+				if arg.Repeat {
+					var val []uint16
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Uint16ListVar(&val)
+				} else {
+					var val uint16
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Uint16Var(&val)
+				}
+			case UINT32:
+				if arg.Repeat {
+					var val []uint32
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Uint32ListVar(&val)
+				} else {
+					var val uint32
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Uint32Var(&val)
+				}
+			case UINT64:
+				if arg.Repeat {
+					var val []uint64
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Uint64ListVar(&val)
+				} else {
+					var val uint64
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).Uint64Var(&val)
+				}
+			case UINT:
+				if arg.Repeat {
+					var val []uint
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).UintsVar(&val)
+				} else {
+					var val uint
+					cmdArg.PreAction(func(_ *kingpin.ParseContext) error {
+						args[arg.Name] = val
+						return nil
+					}).UintVar(&val)
+				}
+			}
+		}
+		copy := alias
+		cmd.Action(func(_ *kingpin.ParseContext) error {
+			return runAlias(&copy, t)
+		})
+	}
+	return nil
+}
